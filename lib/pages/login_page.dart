b5f5fa03a937b11f7cb64365bfe4c07f9cb292af
kartik.waghmare <kartik.waghmare@infoteksoftware.com>
2022-06-06 16:15:41 +0530

    Complete day 5

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   // const LoginPage({Key? key}) : super(key: key);
 
+String name ="";
+bool changeButton =false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -13,13 +20,13 @@ class LoginPage extends StatelessWidget {
           children: [
             Image.asset(
               "assets/images/login.png",
-              fit: BoxFit.cover,
+              height: 150.0,
             ),
             SizedBox(
               height: 20.0,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
@@ -32,7 +39,13 @@ class LoginPage extends StatelessWidget {
                 children: [
                   TextFormField(
                     decoration: InputDecoration(
-                        hintText: "Enter username", labelText: "Username"),
+                        hintText: "Enter username",
+                        labelText: "Username"
+                    ),
+                    onChanged: (value){
+                      name=value;
+                      setState((){});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -42,14 +55,49 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 40.0,
                   ),
-                  ElevatedButton(
-                      onPressed: (){
-                        // print("hi kartik");
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                      child: Text("Login"),
-                    style: TextButton.styleFrom(minimumSize: Size(100,50)),
+
+                  InkWell(
+                    onTap: ()async{
+                      setState((){
+                        changeButton=true;
+                      });
+                      //goto next page with delay
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width:changeButton? 50 : 150,
+                      height: 40,
+                      
+                      alignment: Alignment.center,
+                      child: changeButton?Icon(
+                        Icons.done,
+                        color: Colors.white,
+                      ) :Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                        fontSize: 18.0),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        // shape: changeButton
+                        //   ? BoxShape.circle
+                        //   : BoxShape.rectangle,
+                        borderRadius:
+                        BorderRadius.circular(changeButton? 20 : 8)
+                      ),
+                    ),
                   ),
+                  // ElevatedButton(
+                  //     onPressed: (){
+                  //       // print("hi kartik");
+                  //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //     },
+                  //     child: Text("Login"),
+                  //   style: TextButton.styleFrom(minimumSize: Size(100,50)),
+                  // ),
 
                 ],
               ),
