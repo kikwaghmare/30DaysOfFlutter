04166ec7ee24197775104ec94768ff92f5b70ec7
kartik.waghmare <kartik.waghmare@infoteksoftware.com>
2022-06-06 10:04:54 +0530

    Login page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  //const MyApp({Key? key}) : super(key: key);
-  dynamic name="hello gauravi";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     home: Scaffold(
-      body: Center(
-        child: Text("<\b>Hello worlirfdf $name\t\n\n\n\n\n\b"+name),
-      ),
-    //   Container(
-    //   child: Text("Hello world!!"),
-    // )
-    )
+     home: HomePage(),
+      themeMode: ThemeMode.light,
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+        ),
     );
   }
 }
